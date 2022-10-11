2eea3688c56327e6ada9863baba9d671c38c06e9
Rafał Pieczyk <48286671+rafx666@users.noreply.github.com>
2022-10-11 10:47:15 +0200

    Add login functionality

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,15 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     Key? key,
   }) : super(key: key);
 
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('jestes niezalogowany'),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Zaloguj się',
+                style: TextStyle(fontSize: 22),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(hintText: 'e-mail'),
+              ),
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(hintText: 'hasło'),
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+                onPressed: () async {
+                  try {
+                    await FirebaseAuth.instance.signInWithEmailAndPassword(
+                        email: emailController.text,
+                        password: passwordController.text);
+                  } catch (error) {
+                    print(error);
+                  }
+                },
+                child: const Text('Zaloguj'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
