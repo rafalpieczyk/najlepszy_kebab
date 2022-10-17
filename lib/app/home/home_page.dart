41c0ffcc7a024dcd5bfd4cee79fb72a9fa7c7f89
Rafał Pieczyk <48286671+rafx666@users.noreply.github.com>
2022-10-17 15:17:51 +0200

    add cloud firestore

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -19,37 +20,19 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 50,
+        title: const Text('Najlepszy Kebab na Śląsku'),
+      ),
       body: Builder(builder: (context) {
         if (currentIndex == 0) {
-          return const Center(
-            child: Text('opinie'),
-          );
+          return const RestaurantPageContent();
         }
         if (currentIndex == 1) {
-          return const Center(
-            child: Text('dodaj'),
-          );
+          return const AddOpinionPageContent();
         }
 
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Jesteś zalogowany jako ${widget.user.email}'),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: const Text(
-                  'Wyloguj',
-                ),
-              ),
-            ],
-          ),
-        );
+        return MyAccountPageContent(email: widget.user.email);
       }),
       bottomNavigationBar: BottomNavigationBar(
           currentIndex: currentIndex,
@@ -76,5 +59,115 @@ class _HomePageState extends State<HomePage> {
                 label: 'Moje konto'),
           ]),
     );
+  }
+}
+
+class MyAccountPageContent extends StatelessWidget {
+  const MyAccountPageContent({
+    Key? key,
+    required this.email,
+  }) : super(key: key);
+
+  final String? email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Jesteś zalogowany jako $email'),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: const Text(
+              'Wyloguj',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddOpinionPageContent extends StatelessWidget {
+  const AddOpinionPageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('dodaj'),
+    );
+  }
+}
+
+class RestaurantPageContent extends StatelessWidget {
+  const RestaurantPageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream:
+            FirebaseFirestore.instance.collection('restaurants').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text('Waiting for data'));
+          }
+          final documents = snapshot.data!.docs;
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blue),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              document['name'],
+                              style: const TextStyle(
+                                  fontSize: 18, color: Colors.white),
+                            ),
+                            const SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              document['kebs'],
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          document['rating'].toString(),
+                          style: const TextStyle(
+                              fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ],
+          );
+        });
   }
 }
