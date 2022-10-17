810b8506d75c430b23d913bd342cf33773b7e473
Rafał Pieczyk <48286671+rafx666@users.noreply.github.com>
2022-10-17 15:26:13 +0200

    organize files

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:najlepszy_kebab/app/home/add_opinion/add_opinion_page_content.dart';
+import 'package:najlepszy_kebab/app/home/my_account/my_account_page_content.dart';
+import 'package:najlepszy_kebab/app/home/restaurants/restaurant_page_content.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -59,115 +61,5 @@ class _HomePageState extends State<HomePage> {
                 label: 'Moje konto'),
           ]),
     );
-  }
-}
-
-class MyAccountPageContent extends StatelessWidget {
-  const MyAccountPageContent({
-    Key? key,
-    required this.email,
-  }) : super(key: key);
-
-  final String? email;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Jesteś zalogowany jako $email'),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: const Text(
-              'Wyloguj',
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AddOpinionPageContent extends StatelessWidget {
-  const AddOpinionPageContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('dodaj'),
-    );
-  }
-}
-
-class RestaurantPageContent extends StatelessWidget {
-  const RestaurantPageContent({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream:
-            FirebaseFirestore.instance.collection('restaurants').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const Center(child: Text('Something went wrong'));
-          }
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: Text('Waiting for data'));
-          }
-          final documents = snapshot.data!.docs;
-          return ListView(
-            children: [
-              for (final document in documents) ...[
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.blue),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              document['name'],
-                              style: const TextStyle(
-                                  fontSize: 18, color: Colors.white),
-                            ),
-                            const SizedBox(
-                              height: 6,
-                            ),
-                            Text(
-                              document['kebs'],
-                              style: const TextStyle(
-                                  fontSize: 16, color: Colors.white),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          document['rating'].toString(),
-                          style: const TextStyle(
-                              fontSize: 20, color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ],
-          );
-        });
   }
 }
---
+++ lib/app/home/restaurants/restaurant_page_content.dart
@@ -0,0 +1,67 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class RestaurantPageContent extends StatelessWidget {
+  const RestaurantPageContent({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream:
+            FirebaseFirestore.instance.collection('restaurants').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text('Waiting for data'));
+          }
+          final documents = snapshot.data!.docs;
+          return ListView(
+            children: [
+              for (final document in documents) ...[
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blue),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              document['name'],
+                              style: const TextStyle(
+                                  fontSize: 18, color: Colors.white),
+                            ),
+                            const SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              document['kebs'],
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          document['rating'].toString(),
+                          style: const TextStyle(
+                              fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ],
+          );
+        });
+  }
+}
