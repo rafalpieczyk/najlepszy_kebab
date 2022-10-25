57d93c5f00514facefbccbddfc99f2e18d436e09
Rafał Pieczyk <48286671+rafx666@users.noreply.github.com>
2022-10-25 12:56:10 +0200

    commit

--- lib/app/features/home/restaurants/cubit/restaurants_cubit.dart
+++ lib/app/features/home/restaurants/cubit/restaurants_cubit.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
-
 part 'restaurants_state.dart';
 
 class RestaurantsCubit extends Cubit<RestaurantsState> {
